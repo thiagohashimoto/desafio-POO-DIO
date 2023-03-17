e24aed211754814c95022cd562870a14413be064
Thiago Hashimoto <thiago.hashimoto94@gmail.com>
2023-03-16 21:30:23 -0300

    Criação da classe mentoria e curso

--- src/main/java/br/com/dio/desafio/dominio/Main.java
+++ src/main/java/br/com/dio/desafio/dominio/Main.java
@@ -1,7 +1,28 @@
 package br.com.dio.desafio.dominio;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Programação Orientada a Objetos com Java");
+        curso1.setDescricao("Curso destinado a desenvolver códigos com a prática de Programação Orientada a Objetos");
+        curso1.setCargaHoraria(80);
+
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("JavaScript");
+        curso2.setDescricao("Descrição para curso de JavaScript");
+        curso2.setCargaHoraria(40);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria do Java");
+        mentoria1.setDescricao("Descrição da mentoria do Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
 
     }
 }
